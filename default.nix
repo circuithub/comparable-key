2b815bae8f5103977521bd04b05dd6394ba38900
Oliver Charles <ollie@ocharles.org.uk>
2015-10-14 11:24:34 +0100

    nix: Add default.nix
    Automatically generated by cabal2nix

---
+++ default.nix
@@ -0,0 +1,10 @@
+{ mkDerivation, base, bytestring, hashable, stdenv, text }:
+mkDerivation {
+  pname = "comparable-key";
+  version = "0.0.1";
+  src = ./.;
+  buildDepends = [ base bytestring hashable text ];
+  homepage = "https://github.com/circuithub/comparable-key";
+  description = "Eq, Ord and Hashable for the key part of a data type";
+  license = stdenv.lib.licenses.mit;
+}
