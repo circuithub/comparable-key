080daee7706d232f628a46754a275adbebc75f61
Oliver Charles <ollie@ocharles.org.uk>
2018-07-26 13:52:04 +0100

    Use cabal-sdist.nix

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 mkDerivation {
   pname = "comparable-key";
   version = "0.0.1";
-  src = ./.;
+  src = import ../../nix/cabal-sdist.nix ./.;
   buildDepends = [ base bytestring hashable text ];
   homepage = "https://github.com/circuithub/comparable-key";
   description = "Eq, Ord and Hashable for the key part of a data type";
